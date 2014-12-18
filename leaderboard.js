26daa931f2bbed416c861b9be36debdcfc110466
Calvin Thigpen <cgthigpen@ucdavis.edu>
2014-12-18 11:51:09 -0800

    through chapter 12

--- leaderboard.js
+++ leaderboard.js
@@ -30,15 +30,15 @@ if(Meteor.isClient){
       },
       'click .increment': function(){
         var selectedPlayer = Session.get('selectedPlayer');
-        PlayersList.update(selectedPlayer, {$inc: {score: 5}});
+        Meteor.call('modifyPlayerScore', selectedPlayer, 5)
       },
       'click .decrement': function(){
         var selectedPlayer = Session.get('selectedPlayer');
-        PlayersList.update(selectedPlayer, {$inc: {score: -5}});
+        Meteor.call('modifyPlayerScore', selectedPlayer, -5)
       },
       'click .remove': function(){
         var selectedPlayer = Session.get('selectedPlayer');
-        PlayersList.remove(selectedPlayer);
+        Meteor.call('removePlayerData', selectedPlayer);
       }
     });
 
@@ -46,12 +46,7 @@ if(Meteor.isClient){
       'submit form': function(event) {
         event.preventDefault();
         var playerNameVar = event.target.playerName.value;
-        var currentUserId = Meteor.userId();
-        PlayersList.insert({
-          name: playerNameVar,
-          score: 0,
-          createdBy: currentUserId
-        });
+        Meteor.call('insertPlayerData', playerNameVar);
       }
     })
 }
@@ -60,5 +55,22 @@ if(Meteor.isServer){
   Meteor.publish('thePlayers', function(){
     var currentUserId = this.userId;
     return PlayersList.find({createdBy: currentUserId})
+  });
+
+  Meteor.methods({
+    'insertPlayerData': function(playerNameVar){
+      var currentUserId = Meteor.userId();
+      PlayersList.insert({
+        name: playerNameVar,
+        score: 0,
+        createdBy: currentUserId
+      });
+    },
+    'removePlayerData': function(selectedPlayer){
+      PlayersList.remove(selectedPlayer);
+    },
+    'modifyPlayerScore': function(selectedPlayer, scoreValue){
+      PlayersList.update(selectedPlayer, {$inc: {score: scoreValue}});
+    }
   });
 }
