95e640ff6c1833d66639e731158a5ca19e0d22f5
Calvin Thigpen <cgthigpen@ucdavis.edu>
2014-12-18 07:55:45 -0800

    second commit to catch up through chapter 5

--- leaderboard.js
+++ leaderboard.js
@@ -1,1 +1,18 @@
 PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+    // this code only runs on the client
+    
+    Template.leaderboard.helpers({
+        'player': function(){
+            return PlayersList.find()
+        },
+        'otherHelperFunction': function(){
+            return "Some other function"
+        }
+    });
+}
+
+if(Meteor.isServer){
+    // this code only runs on the server
+}
