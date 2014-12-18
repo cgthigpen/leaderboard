cddadbc0432d0942909ad88a8fdef2f7d53e0606
Calvin Thigpen <cgthigpen@ucdavis.edu>
2014-12-18 08:35:43 -0800

    through chapter 8

--- leaderboard.js
+++ leaderboard.js
@@ -5,7 +5,7 @@ if(Meteor.isClient){
 
     Template.leaderboard.helpers({
         'player': function(){
-            return PlayersList.find()
+            return PlayersList.find({}, {sort: {score: -1, name: 1} })
         },
         'selectedClass': function(){
             var playerId = this._id;
@@ -13,6 +13,10 @@ if(Meteor.isClient){
             if(playerId == selectedPlayer){
               return "selected"
             }
+        },
+        'showSelectedPlayer': function(){
+          var selectedPlayer = Session.get('selectedPlayer');
+          return PlayersList.findOne(selectedPlayer)
         }
     });
 
@@ -20,8 +24,16 @@ if(Meteor.isClient){
       'click .player': function(){
         var playerId = this._id;
         Session.set('selectedPlayer', playerId);
+      },
+      'click .increment': function(){
+        var selectedPlayer = Session.get('selectedPlayer');
+        PlayersList.update(selectedPlayer, {$inc: {score: 5}});
+      },
+      'click .decrement': function(){
+        var selectedPlayer = Session.get('selectedPlayer');
+        PlayersList.update(selectedPlayer, {$inc: {score: -5}});
       }
-    })
+  })
 }
 
 if(Meteor.isServer){
